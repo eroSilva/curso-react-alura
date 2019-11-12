db1551f7a0b08b10abaeae7a34b573d1431d7014
Erô Silva <eronildo.silva@outlook.com>
2019-11-12 13:22:30 -0300

    Cria State para armazenar o array de autores

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Tabela from './Tabela';
 
 import './App.css';
 
-function App() {
-  const autores = [
-    {
-      nome: 'Paulo',
-      livro: 'React',
-      preco: '1000'
-    },
-    {
-      nome: 'Daniel',
-      livro: 'Java',
-      preco: '99'
-    },
-    {
-      nome: 'Marcos',
-      livro: 'Design',
-      preco: '150'
-    },
-    {
-      nome: 'Bruno',
-      livro: 'DevOps',
-      preco: '100'
-    }
-  ]
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ]
+  };
 
-  return (
-    <div className="App">
-      <Tabela autores={autores} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="App">
+        <Tabela autores={this.state.autores} />
+      </div>
+    );
+  };
+};
 
 export default App;
